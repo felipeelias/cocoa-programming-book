e03cbaeee3b6074b00d74493ff7d090ef6c0c416
Felipe Elias Philipp <felipeelias@gmail.com>
2011-06-01 23:45:58 +0100

    organizing with pragma mark

--- MobileBrowser/Classes/MobileBrowserViewController.m
+++ MobileBrowser/Classes/MobileBrowserViewController.m
@@ -12,6 +12,8 @@
 
 @synthesize webView, address, backButton, forwardButton, activity;
 
+#pragma mark Helper methods
+
 -(void) disableWebView { 
 	self.webView.userInteractionEnabled = NO;
 	self.webView.alpha = 0.25;
@@ -31,13 +33,17 @@
 -(void) resetButtons:(UIWebView *) theWebView { 
 	[self.backButton setEnabled:[theWebView canGoBack]]; 
 	[self.forwardButton setEnabled:[theWebView canGoForward]];
-} 
+}
+
+#pragma mark Initialization
 
 - (void) viewDidLoad {
 	[super viewDidLoad];
 	self.address.text = @"http://google.com";
 	[self loadURLFromTextField];
 }
+
+#pragma mark WebView delegate methods
 
 -(void)webViewDidStartload:(UIWebView *)theWebView {
 	[self.activity startAnimating];
@@ -51,6 +57,8 @@
 	self.address.text = [[self.webView.request URL] absoluteString];
 }
 
+#pragma mark Text Field delegate methods
+
 -(void)textFieldDidBeginEditing:(UITextField *)textField {
 	[self disableWebView];
 }
@@ -60,6 +68,8 @@
 	[textField resignFirstResponder];
 	return YES;
 }
+
+#pragma mark Memory Management
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
