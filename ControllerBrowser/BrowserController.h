b3813a1f77832c5967efcd6c4c46430193065344
Felipe Elias Philipp <felipeelias@gmail.com>
2011-06-01 16:26:40 +0100

    add BrowserController class an instantiate in IB

---
+++ ControllerBrowser/BrowserController.h
@@ -0,0 +1,16 @@
+//
+//  BrowserController.h
+//  ControllerBrowser
+//
+//  Created by Felipe Elias Philipp on 01/06/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface BrowserController : NSObject {
+
+}
+
+@end
