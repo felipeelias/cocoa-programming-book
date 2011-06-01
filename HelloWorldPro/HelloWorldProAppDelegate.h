9be4003add2af4b6c3d43a18ac6a8bb85ca3580f
Felipe Elias Philipp <felipeelias@gmail.com>
2011-06-01 15:21:41 +0100

    Chap7 app initial files

---
+++ HelloWorldPro/HelloWorldProAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  HelloWorldProAppDelegate.h
+//  HelloWorldPro
+//
+//  Created by Felipe Elias Philipp on 01/06/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface HelloWorldProAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ HelloWorldPro/HelloWorldProAppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  HelloWorldProAppDelegate.m
+//  HelloWorldPro
+//
+//  Created by Felipe Elias Philipp on 01/06/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "HelloWorldProAppDelegate.h"
+
+@implementation HelloWorldProAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application 
+}
+
+@end
