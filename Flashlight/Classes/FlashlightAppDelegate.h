b16572e2728ccdfafd03eb8443a0454cf9074d34
Felipe Elias Philipp <felipeelias@gmail.com>
2011-06-01 01:31:37 +0100

    Flashlight iPhone app

---
+++ Flashlight/Classes/FlashlightAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  FlashlightAppDelegate.h
+//  Flashlight
+//
+//  Created by Felipe Elias Philipp on 01/06/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlashlightAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
