e3ded842b04dcaa210a16dc6f0698e0b4c3ce773
Felipe Elias Philipp <felipeelias@gmail.com>
2011-06-01 01:21:43 +0100

    simulates a memory leak

--- HelloWorld/HelloWorldAppDelegate.m
+++ HelloWorld/HelloWorldAppDelegate.m
@@ -11,36 +11,9 @@
 
 @implementation HelloWorldAppDelegate
 
-@synthesize window;
-
-- (NSTextField *) labelWithText: (NSString *) labelText {
-	NSRect labelFrame = NSMakeRect(20, 20, 540, 50);
-	NSTextField *label = [[NSTextField alloc] initWithFrame:labelFrame];
-	[label setEditable:NO];
-	[label setSelectable:NO];
-	[label setAlignment:NSCenterTextAlignment];
-	[label setFont:[NSFont boldSystemFontOfSize:36]];
-	[label setStringValue:labelText];
-	return label;
-}
-
-- (Greeter *) greeterFor:(NSString *) personName {
-	return [[Greeter alloc] initWithName:personName];
-}
-
--(void) setUpperCaseName:(Greeter *) greeter {
-	NSLog(@"The name originally was %@", greeter.name);
-	greeter.name = [greeter.name uppercaseString];
-	NSLog(@"The name is now %@", greeter.name);
-}
-
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-	Greeter *greeter = [self greeterFor:@"Maggie"];
-	NSLog(@"Greeter: %@", greeter);
-	NSLog(@"This error occurred in %@ at line %d in files %s", NSStringFromSelector(_cmd), __LINE__, __FILE__);
-	NSTextField *label = [self labelWithText:[greeter greeting]];
-	[self setUpperCaseName:greeter];
-	[[self.window contentView] addSubview:label];
+	Greeter *host = [[Greeter alloc] initWithName:@"Maggie"];
+	NSLog(@"Greeter %@", host);
 }
 
 @end
