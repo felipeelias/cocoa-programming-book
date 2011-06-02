ee1eaee9ccc910b4186f3e941569181f5f2a8705
Felipe Elias Philipp <felipeelias@gmail.com>
2011-06-03 00:11:06 +0100

    add MyWindowController to control the window nib

---
+++ HelloApplication/MyWindowController.h
@@ -0,0 +1,16 @@
+//
+//  MyWindowController.h
+//  HelloApplication
+//
+//  Created by Felipe Elias Philipp on 03/06/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface MyWindowController : NSWindowController {
+
+}
+
+@end
---
+++ HelloApplication/MyWindowController.m
@@ -0,0 +1,14 @@
+//
+//  MyWindowController.m
+//  HelloApplication
+//
+//  Created by Felipe Elias Philipp on 03/06/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "MyWindowController.h"
+
+
+@implementation MyWindowController
+
+@end
