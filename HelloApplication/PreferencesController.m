9d68132866705464051945b9e1527e4b06e852b0
Felipe Elias Philipp <felipeelias@gmail.com>
2011-06-04 13:22:45 +0100

    add chooseView action and update the radios when view open

--- HelloApplication/PreferencesController.m
+++ HelloApplication/PreferencesController.m
@@ -11,10 +11,14 @@
 
 @implementation PreferencesController
 
-@synthesize loadSavedRemovedAppsCheckbox;
+@synthesize loadSavedRemovedAppsCheckbox, viewGroup;
 
 - (IBAction) toggleLoadSavedRemovedAppsCheckbox: (id) sender {
   [[NSUserDefaults standardUserDefaults] setBool:[self.loadSavedRemovedAppsCheckbox state] forKey:@"LoadSavedRemovedApps"];
+}
+
+- (IBAction) chooseView: (id) sender {
+  [[NSUserDefaults standardUserDefaults] setObject:[NSNumber numberWithInt:[[sender selectedCell] tag]] forKey:@"TagForView"];
 }
 
 - (void) windowWillClose: (NSNotification *) notification {
@@ -23,6 +27,7 @@
 
 - (void) awakeFromNib {
   [self.loadSavedRemovedAppsCheckbox setState:[[NSUserDefaults standardUserDefaults] boolForKey:@"LoadSavedRemovedApps"]];
+  [self.viewGroup selectCellWithTag:[[[NSUserDefaults standardUserDefaults] objectForKey:@"TagForView"] intValue]];
 }
 
 @end
